8ae44139962001a6db77c24c918ab9b7458df033
Andre Hora <andrehoraa@gmail.com>
2023-08-07 14:56:23 -0300

    working on exporter

---
+++ pathspotter/utils.py
@@ -0,0 +1,7 @@
+import os
+
+
+def export_dir(local_dir, file_origin):
+    file_path = os.path.dirname(file_origin)
+    head_path = os.path.split(file_path)[0]
+    return os.path.join(head_path, local_dir)
